de3476853980ee4793c7da5e5ef1f0b73aa90b6a
egbeago <georgefredrick8@gmail.com>
2020-01-07 12:14:09 +0100

    another

--- react-front/src/user/Users.js
+++ react-front/src/user/Users.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {list} from "./apiUser"
-import defaultProfile from "./images/avater.jpeg"
+import defaultProfile from "../images/avater.jpg"
 
 class Users extends Component {
     constructor() {
@@ -25,7 +25,7 @@ class Users extends Component {
                     {users.map((user, i) => 
                        ( 
                         <div className="card col-md-4" key={i}>
-                        <img className="card-img-top" src={defaultProfile} alt={user.name}  />
+                        <img className="card-img-top" src={defaultProfile} alt={user.name} style={{width: "100%", height: "15vw", objectFit: "cover"}} />
                         <div className="card-body">
                           <h5 className="card-title">{user.name}</h5>
                           <p className="card-text">
